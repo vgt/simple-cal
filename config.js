78adb76a069b1bdf168d95f6415ddb266e13ddb6
vgt <github@davidvogt.de>
2021-09-29 22:47:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var calendar = [
-    { 'url': 'webcal://www.dreier.no/ical/weeknumbers.ics', 'color': '#00FF00' },
-    { 'url': 'webcal://www.kouzos.com/ical/DVD.ics', 'color': '#8888FF' }
+    { 'url': '...', 'color': '#00FF00' },
+    { 'url': '...', 'color': '#8888FF' }
 ]
